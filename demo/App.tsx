9ba09613508ccdc895790ba2f9391c6c1452a87d
lee_jjoon <whitour@gmail.com>
2020-03-20 23:39:05 +0900

    feat: ios animation complete

--- demo/App.tsx
+++ demo/App.tsx
@@ -9,22 +9,24 @@ import {
 // import DatePicker, { Mode } from 'simple-react-native-datepicker';
 // import DateTimePicker from '@react-native-community/datetimepicker
 import DatePciker from './datepicker/newDatepikcer';
-
+    
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { date: new Date(), dateTime: '', time: '', visible: false };
 	}
 	public render() {
+		console.log(this.state.visible)
 		return (
 			<SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 				<View>
 					<Text style={{ marginBottom: 20 }}>Hello! simple date picker!</Text>
 					<Text style={{ marginBottom: 20 }}>time is {String(this.state.date)}</Text>
-					<Button onPress={() => this.setState({ visible: true })} title='show' />
-					<DatePciker visible={true} onDateChange={(date) => this.setState({ date })} date={this.state.date} />
-					{/* <DatePicker mode={Mode.datetime} onDateChange={(dateTime) => this.setState({ dateTime })} date={this.state.dateTime} placeholder='click here' />
-					<DatePicker mode={Mode.time} onDateChange={(time) => this.setState({ time })} date={this.state.time} is24Hour={true} placeholder='click here' /> */}
+					<Button onPress={() => {
+						console.log(this.state.visible)
+						this.setState({ visible: !this.state.visible });
+						}} title='show' />
+					<DatePciker visible={this.state.visible} onDateChange={(date) => this.setState({ date })} date={this.state.date} />
 				</View>
 			</SafeAreaView>
 		);
